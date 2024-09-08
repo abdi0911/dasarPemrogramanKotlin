5e4f3ce1605d24bc8874e48cb45b0c28cd46cdea
abdi0911 <abdijr811@gmail.com>
2024-09-08 16:09:22 +0700

    Add Literal.kt.kt

---
+++ src/main/kotlin/tipeData.kt
@@ -0,0 +1,44 @@
+fun main(args: Array<String>) {
+//    var charA : Char = 'A'
+////    println(charA == 65)
+//    println(charA.isUpperCase())
+//    println(charA.isLowerCase())
+//    println(charA.isDigit())
+//    println(charA.toLowerCase())
+//    val strA: String = charA.toString()
+//    println("Kini charA sudah menjadi String = "+strA)
+
+//    //Boolean
+//        var varBool : Boolean = true
+//        if(varBool)
+//            println("varBool bernilai " + varBool)
+//
+//    //tambahan kode
+//    var varInt : Int = 1
+////    if(varInt) //harus dibandingkan != 0
+//        println("varInt bernilai " + varInt)
+
+
+    //String (Array of Character)
+
+//        val myName: String = "A b d i"
+//        for(chr in myName){
+//            print(chr)
+//        }
+//        print('\n')
+//
+//    // tambahan
+//    val s = "abc" + 1
+//    println(s + "def")
+
+//
+    var text = """
+ |Tell me and I forget.
+ |Teach me and I remember.
+ |Involve me and I learn.
+ |(Benjamin Franklin)
+ """.trimMargin()
+    println(text)
+
+
+}
